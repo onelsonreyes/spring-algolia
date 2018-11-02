4428b126a07b0e25f1812ecb91a39c9c61d0c967
onelsonreyes <nelson@osigu.com>
2018-11-02 12:22:21 -0600

    add initial configuration

---
+++ src/main/java/com/karadak/springalgolia/SpringAlgoliaApplication.java
@@ -0,0 +1,12 @@
+package com.karadak.springalgolia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlgoliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlgoliaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/karadak/springalgolia/SpringAlgoliaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.karadak.springalgolia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAlgoliaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
